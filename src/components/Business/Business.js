28f29d8accb19e99c43eaa6a77c77f11c34cd65c
shakhawatfci <94shakha.18@gamil.com>
2020-05-22 23:35:34 +0600

    3rd step with step handling

---
+++ src/components/Business/Business.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Business.css';
+
+class Business extends React.Component {
+    // state = {  }
+    render() { 
+  return (
+        <div class="Business">
+  <div class="image-container">
+    <img src={this.props.business.imageSrc} alt=''/>
+  </div>
+        <h2>{this.props.business.name}</h2>
+  <div class="Business-information">
+    <div class="Business-address">
+        <p>{this.props.business.address}</p>
+      <p>{this.props.business.city}</p>
+      <p>{this.props.business.state} {this.props.business.zipCode}</p>
+    </div>
+    <div class="Business-reviews">
+      <h3>{this.props.business.category}</h3>
+      <h3 class="rating">{this.props.business.rating} stars}</h3>
+      <p>{this.props.business.reviewCount} reviews</p>
+    </div>
+  </div>
+</div>
+        );
+    }
+}
+ 
+export default Business;
